b5ec72a96cf52149dc14c017ac6495005f19b04c
chalagit config --global user.email chaelamohamed182@gmail.com <chaelamohamed182@gmail.com>
2025-12-17 15:16:06 +0200

    update it

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("programming koo hojii jalqeera ");
+let maqaa = "caalaa";
+let umrii = 14;
+let biyyakoo = "ciro";
+console.log("Ani maqaan koo: ", maqaa, "dha", "umriin koo", umrii, "lafan itti dhaladhe", biyyakoo);
+// calculation
+console.log("calculation \n\n");
+let umriinkoowaggaadhufu = umrii
+let umriikonkalaachisuma = 18 - umrii
+console.log("waggaa dhufu umriin koo:", umriinkoowaggaadhufu, "ta'a");
+console.log("konkolachisuueegaluuf wagga:", umriikonkalaachisuma, "tu siif hafa");
+
