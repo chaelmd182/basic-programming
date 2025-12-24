de235c3ca4b8d8c97269d79dca01613f82f8d04e
chalagit config --global user.email chaelamohamed182@gmail.com <chaelamohamed182@gmail.com>
2025-12-24 18:53:23 +0200

    update this part
    update

--- script1.js
+++ script1.js
@@ -1,5 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('page loaded ready for java')
 }
-    
 )
+const changetextBtn = document.getElementById('changetextBtn');
+console.log(changetextBtn);
+ 
+const dispalytext = document.getElementById('dispalytext')
+changetextBtn.addEventListener('click',
+    function () {
+        console.log('button is clicked')   
+       dispalytext.textContent=('java script barreeffama kana jijjiiri')
+     }
+)
+const nameinput = document.getElementById('nameinput')
+const ageInput = document.getElementById('ageInput')
+const updateinfoBtn = document.getElementById('updateinfoBtn')
+const waayeekee = document.getElementById('waayeekee')
+updateinfoBtn.addEventListener('click', function () {
+    const nameinput = document.getElementById('nameinput') 
+    const ageInput = document.getElementById('ageInput')
+    waayeekee.textContent= "maqaa  kee " + nameinput.value + " umree kee ammoo " + ageInput.value + " dha!"
+  })
--- typescript.js
+++ typescript.js
@@ -1,4 +1,9 @@
- function showRegister(){
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('page loading');
+  })
+
+
+function showRegister() {
   document.getElementById("loginBox").style.display = "none";
   document.getElementById("registerBox").style.display = "block";
 }
@@ -7,3 +12,13 @@ function showLogin(){
   document.getElementById("registerBox").style.display = "none";
   document.getElementById("loginBox").style.display = "block";
 }
+const textbtn = document.getElementById('textbtn');
+const text1btn = document.getElementById('text1btn');
+const ext2btn = document.getElementById('ext2btn');
+const text3btn = document.getElementById('text3btn');
+const text4btn = document.getElementById('text4btn');
+const showtext = document.getElementById('showtext');
+const cleckbtn = document.getElementById('cleckbtn');
+cleckbtn.addEventListener('click', function () {
+  showtext.textContent="please fillout this"
+})
